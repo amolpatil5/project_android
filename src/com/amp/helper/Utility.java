3fe7ce33a2506530e3f73c47ef65982bbe3ad7a8
amolpatil5 <patilamol.30@gmail.com>
2015-03-25 23:28:38 +0530

    language translation change

--- src/com/amp/helper/Utility.java
+++ src/com/amp/helper/Utility.java
@@ -1,12 +1,13 @@
 package com.amp.helper;
 
 import java.util.Locale;
-
 import com.twostars.syadav.R;
-
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -15,21 +16,35 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.util.DisplayMetrics;
 
-
-
-
-
 public class Utility 
 {
 	private static Utility ut; 
 	private Context mcontext;
+	static SharedPreferences pref;
+	static Editor editor; 
+	private static final String PREF_NAME = "YADAVPREF";
+	public static final String KEY_NAME = "LANGUAGENAME";
 
 	public Utility(Context context) {
 		mcontext = context;
+		pref = context.getSharedPreferences(PREF_NAME, Activity.MODE_PRIVATE);
+		editor = pref.edit();
+	}
+	public static void storeCurrentLanuage(String name)
+	{
+		editor.putString(KEY_NAME, name);
+		editor.commit();
+	}
+
+	// Get Login State
+	public static String getCurrentLanguage()
+	{
+		return pref.getString(KEY_NAME, "");
 	}
 
 	public static Utility getFontManager(Context context) {
-		if (ut == null) {
+		if (ut == null)
+		{
 			ut = new Utility(context);
 		}
 		return ut;
@@ -57,14 +72,6 @@ public class Utility
 
 	public static void showNetworkConnectionError(final Context context)
 	{
-		//			  AlertDialog alert = new AlertDialog.Builder(context).create();
-		//	            alert.setTitle("Connection Error");
-		//	          //  String errorMsg = getResources().getString(R.string.NETWORK_ERROR);
-		//	            alert.setMessage("Unable to connect,Please check your internet connection.");
-		//	            alert.setCancelMessage(null);
-		//	            alert.show();
-
-
 		AlertDialog.Builder builder = new AlertDialog.Builder(context);
 		builder.setCancelable(true);
 		builder.setTitle("Connection Error");
@@ -73,19 +80,13 @@ public class Utility
 		builder.setPositiveButton("OK",null);
 		AlertDialog alert = builder.create();
 		alert.show();
-
 	}
-	
 	public static void updateLocaleWithLanguage(final Context context, String language_code)
 	{
 		Resources res = context.getResources();
-	    // Change locale settings in the app.
-	    DisplayMetrics dm = res.getDisplayMetrics();
-	    android.content.res.Configuration conf = res.getConfiguration();
-	    conf.locale = new Locale(language_code.toLowerCase());
-	    res.updateConfiguration(conf, dm);
+		DisplayMetrics dm = res.getDisplayMetrics();
+		android.content.res.Configuration conf = res.getConfiguration();
+		conf.locale = new Locale(language_code.toLowerCase());
+		res.updateConfiguration(conf, dm);
 	}
-
-	
-	
 }
